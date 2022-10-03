deea2fcd5fcbcf5a13d9ba033bab21065a47979b
tabascum <marques_vasco@yahoo.es>
2022-10-03 16:24:36 +0100

    Upload server with the local use of Postman

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const { application } = require("express");
 const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const log = (req, res, next) => {
+  console.log(req.body);
+  console.log(`Data: ${Date.now()}`);
+  next();
+};
+
+app.use(log);
+
+//set express routes
+app.get("/", (req, res) => {
+  res.send("<h1>To Do List</h1>");
+});
+
+app.get("/json", (req, res) => {
+  console.log(req.body);
+});
+
+//start the server
+app.listen(3000, () => {
+  console.log("Server is online");
+});
